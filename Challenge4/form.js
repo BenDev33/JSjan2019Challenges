e66239b67017d466e3a96e45c4736e3037a229ad
Ben D <macuser@Macs-MacBook-Pro.local>
2019-01-21 19:02:03 -0500

    form validation

--- Challenge4/form.js
+++ Challenge4/form.js
@@ -3,4 +3,23 @@
 
 
 // This should alert the user if they hit submit without all of the fields filled in, saying, "Please fill in all your fields".  It should also check the password and, if they do not match, the user should be alerted with "Your passwords MUST match". If all of the fields are filled in, and the passwords match, when the submit button is clicked, the button should become disabled.
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let cpassword = document.getElementById("cpassword");
+let form = document.getElementById("myform");
 
+
+form.addEventListener('submit', submit);
+
+function submit(e) {  
+    e.preventDefault();
+    if (username.value === "" || password.value === "" || cpassword === "") {
+        alert("Please fill in all your fields");
+        return false;
+    } else if (password.value !== cpassword.value) {
+        alert("Your passwords must match");
+        return false;
+    }   else {
+        return true;
+    }
+    };
